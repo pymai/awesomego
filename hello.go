3790816cfe50817a16dbac56c8b4d86859f92eb3
awker <>
2021-05-22 04:01:41 +0800

    使用 json 库读取 json 文件

--- hello.go
+++ hello.go
@@ -1,14 +1,13 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/pymai/awesomego/ijson"
-)
+import "github.com/pymai/awesomego/ijson"
 
 func main() {
-	fmt.Println("Hello World!")
+	//fmt.Println("Hello World!")
 
 	// 创建 json 文件
-	ijson.WriteJson()
+	//ijson.WriteJson()
+
+	// 读取 json 文件
+	ijson.ReadJson()
 }
---
+++ ijson/read-json.go
@@ -0,0 +1,36 @@
+package ijson
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+func ReadJson() {
+	path, err := os.Getwd()
+	if err != nil {
+		// 如果 return 语句使用在普通的 函数 中，则表示跳出该函数，不再执行函数中 return 后面的代码，可以理解成终止函数。
+		// 如果 return 语句使用在 main 函数中，表示终止 main 函数，也就是终止程序的运行。
+		fmt.Println("无法获取当前路径", err.Error())
+		return
+	} else {
+		fmt.Println(path)
+	}
+	filePtr, err := os.Open(path + "/ijson/info.json")
+	if err != nil {
+		// 文件打开失败 open /Users/khmai/awesomego/ijson/info.json: no such file or directory
+		fmt.Println("文件打开失败", err.Error())
+		return
+	}
+	defer filePtr.Close()
+	var info []Website
+	// 创建 json 解码器
+	decoder := json.NewDecoder(filePtr)
+	err = decoder.Decode(&info)
+	if err != nil {
+		fmt.Println("解码失败", err.Error())
+	} else {
+		fmt.Println("解码成功")
+		fmt.Println(info)
+	}
+}

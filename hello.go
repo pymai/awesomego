383841f8cac6d573cd08486a7b7a7a6c3809d9b8
awker <>
2021-06-01 01:16:31 +0800

    使用 log 包来输出格式化的日志

--- hello.go
+++ hello.go
@@ -1,6 +1,25 @@
 package main
 
-import "github.com/pymai/awesomego/ijson"
+import (
+	"log"
+	"os"
+
+	"github.com/pymai/awesomego/ijson"
+)
+
+// init() 在 main 函数之前执行，可以用来初始化日志格式
+func init() {
+	log.SetPrefix("[hello.go] ")
+	file, err := os.OpenFile("./hello.go.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0755)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.SetOutput(file)
+	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds | log.Llongfile)
+
+	// 最后实现的格式
+	// [hello.go] 2021/06/01 01:11:04.940030 /Users/khmai/awesomego/hello.go:30: 读取 json 文件
+}
 
 func main() {
 	//fmt.Println("Hello World!")
@@ -9,5 +28,8 @@ func main() {
 	//ijson.WriteJson()
 
 	// 读取 json 文件
+	//log.Fatalln("读取 json 文件") // 输出信息，并且 "with exit code 1"
+	//log.Panicln("读取 json 文件") // 输出信息，并且 "with exit code 2"
+	log.Println("读取 json 文件")
 	ijson.ReadJson()
 }
